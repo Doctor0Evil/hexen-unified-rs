5aca09bd59803f6ce18d56d77a08f355ba7d2d5f
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-02-04 18:36:22 -0700

    Create iptables_hardening.sh

---
+++ scripts/iptables_hardening.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
