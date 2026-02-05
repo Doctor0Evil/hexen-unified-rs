5a9412735bff857b1e6adac96aa3a64070ce6308
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-02-04 18:31:08 -0700

    Create registry.rs

---
+++ crates/hexen-evidence-registry/src/registry.rs
@@ -0,0 +1,6 @@
+use bioscale_upgrade_store::types::EvidenceTag;
+
+pub static DEFAULT_BIOPHYS_EVIDENCE: &[EvidenceTag] = &[
+    EvidenceTag("a1f3c9b2".into()),
+    EvidenceTag("8f09d5ee".into()),
+];
