bae86ca0445edbb444a5a3e0d3540ccefad47fa3
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-02-04 18:31:30 -0700

    Create BLEScannerActivity.java

---
+++ clients/android/BLEScannerActivity.java
@@ -0,0 +1,11 @@
+package bci.hexen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BLEScannerActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
